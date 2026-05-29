82824f031a25bec1fe38fbb032026864df8142ad
orxfun <orx.ugur.arikan@gmail.com>
2026-05-30 00:21:10 +0200

    define ptr_mut

--- src/col.rs
+++ src/col.rs
@@ -607,6 +607,16 @@ where
         unsafe { self.con_pinned_vec.clear(prior_len) };
         self.state = S::new_for_con_pinned_vec(&self.con_pinned_vec, 0);
     }
+
+    #[inline(always)]
+    /// Returns a mutable reference to the element at the index-th position.
+    ///
+    /// # Safety
+    ///
+    /// Reading the pointer before writing its value will result in UB.
+    pub unsafe fn ptr_mut(&self, idx: usize) -> *mut T {
+        unsafe { self.con_pinned_vec.get_ptr_mut(idx) }
+    }
 }
 
 // HELPERS
